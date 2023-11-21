4a5ca0d842f200c71ba6db8b3194d7d9a2c81b5a
Matt Clagett <mattrclagett@gmail.com>
2023-11-21 14:21:23 -0800

    Exports

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react-refresh/only-export-components */
+// In src/index.ts
+
+// Exporting components
+export * from "./stories/Avatar"
+
+// Exporting hooks
+export * from "./hooks/useOnClickOutside"
